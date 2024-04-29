afed1204ebe5c78f5e05b69633b3762a5e17d6ff
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-29 15:09:43 -0300

    test

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -2,7 +2,7 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper)
-    
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 }
   }
   init() {
     if (this.slide && this.wrapper) {
@@ -20,16 +20,26 @@ export default class Slide {
     this.wrapper.addEventListener("mousedown", this.onStart)
     this.wrapper.addEventListener("mouseup", this.onEnd)
   }
-  onStart() {
-    console.log("mousedown")
+  onStart(event) {
     this.wrapper.addEventListener("mousemove", this.onMove)
+    this.dist.startX = event.clientX
   }
   onMove(event) {
     event.preventDefault()
-    console.log("moveu")
+    const finalPosition = this.updatePosition(event.clientX)
+    this.moveSlide(finalPosition)
   }
   onEnd() {
-    console.log("mouseup")
     this.wrapper.removeEventListener("mousemove", this.onMove)
+    this.dist.finalPosition = this.dist.movePosition
+  }
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement
+  }
+  moveSlide(distX) {
+    this.dist.movePosition = distX
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+    console.log(this.dist)
   }
 }
