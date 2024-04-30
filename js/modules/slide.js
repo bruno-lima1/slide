f6a5345b69cc26e927a048c4160586ffa302b64f
Bruno César <brunocesar.tenorio1@gmail.com>
2024-04-30 12:01:00 -0300

    test

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,44 +1,44 @@
 export default class Slide {
   constructor(slide, wrapper) {
-    this.slide = document.querySelector(slide)
-    this.wrapper = document.querySelector(wrapper)
-    this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
   }
   init() {
     if (this.slide && this.wrapper) {
-      this.bindEvents()
-      this.addSlideEvents()
+      this.bindEvents();
+      this.addSlideEvents();
     }
     return this;
   }
   bindEvents() {
-    this.onStart = this.onStart.bind(this)
-    this.onMove = this.onMove.bind(this)
-    this.onEnd = this.onEnd.bind(this)
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
   }
   addSlideEvents() {
-    this.wrapper.addEventListener("mousedown", this.onStart)
-    this.wrapper.addEventListener("mouseup", this.onEnd)
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
   }
   onStart(event) {
-    this.wrapper.addEventListener("mousemove", this.onMove)
-    this.dist.startX = event.clientX
+    this.wrapper.addEventListener("mousemove", this.onMove);
+    this.dist.startX = event.clientX;
   }
   onMove(event) {
-    event.preventDefault()
-    const finalPosition = this.updatePosition(event.clientX)
-    this.moveSlide(finalPosition)
+    event.preventDefault();
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
   onEnd() {
-    this.wrapper.removeEventListener("mousemove", this.onMove)
-    this.dist.finalPosition = this.dist.movePosition
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
   updatePosition(clientX) {
     this.dist.movement = (this.dist.startX - clientX) * 1.6;
-    return this.dist.finalPosition - this.dist.movement
+    return this.dist.finalPosition - this.dist.movement;
   }
   moveSlide(distX) {
-    this.dist.movePosition = distX
-    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 import Slide from "./modules/slide.js";
 const slide = new Slide("[data-wrapper]", "[data-slide]");
-slide.init()
+slide.init();
