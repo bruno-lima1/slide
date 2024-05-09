c133593929494707e48f41dc4565d6a3b2cf51cf
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-09 19:45:31 -0300

    fixing error

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@
 
 
 
-// import Slide from "./slide.js";
-// const slide = new Slide("[data-wrapper]", "[data-slide]");
-// slide.init()
-// slide.changeSlide(0)
+import Slide from "./slide.js";
+const slide = new Slide("[data-wrapper]", "[data-slide]");
+slide.init()
+slide.changeSlide(0)
