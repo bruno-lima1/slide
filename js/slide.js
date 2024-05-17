be636e90682a51a3c700fae0c329c4dff5e8dc5d
Bruno César <brunocesar.tenorio1@gmail.com>
2024-05-17 12:50:40 -0300

    adding highlight in main slide

--- js/slide.js
+++ js/slide.js
@@ -42,9 +42,9 @@
 
 
 
-export default class SLide {
+export default class Slide {
   constructor(wrapper, slide) {
-    this.wrapper = document.querySelector(wrapper)
+    this.wrapper = document.querySelector(wrapper);
     this.slide = document.querySelector(slide)
     this.values = {
       startPosition: 0,
@@ -52,43 +52,44 @@ export default class SLide {
       distance: 0,
       endPosition: 0
     }
+    this.active = "active"
   }
   init() {
     if (this.wrapper && this.slide) {
-      this.bindEvents();
+      this.bindEvents()
       this.addEvents()
       this.centralizeSlide(0)
     }
     return this;
   }
   bindEvents() {
-    this.onStart = this.onStart.bind(this)
+    this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this)
     this.onEnd = this.onEnd.bind(this)
   }
   addEvents() {
-    this.wrapper.addEventListener("mousedown", this.onStart)
-    this.wrapper.addEventListener("touchstart", this.onStart)
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("touchstart", this.onStart);
     this.wrapper.addEventListener("mouseup", this.onEnd)
     this.wrapper.addEventListener("touchend", this.onEnd)
   }
   onStart(event) {
-    let movetype
+    event.preventDefault()
+    let movetype;
     if (event.type === "mousedown") {
-      this.values.startPosition = event.clientX
       movetype = "mousemove"
+      this.values.startPosition = event.clientX
     } else if (event.type === "touchstart") {
-      this.values.startPosition = event.changedTouches[0].clientX
       movetype = "touchmove"
+      this.values.startPosition = event.changedTouches[0].clientX
     }
     this.wrapper.addEventListener(movetype, this.onMove)
     return this.transition(false)
   }
   onMove(event) {
-    event.preventDefault()
     const clientX = event.type === "mousemove"
-      ? event.clientX
-      : event.changedTouches[0].clientX
+    ? event.clientX
+    : event.changedTouches[0].clientX
     this.values.movement = (this.values.startPosition - clientX) * 1.6;
     this.values.distance = this.values.endPosition - this.values.movement
     return this.moveSlide(this.values.distance)
@@ -110,37 +111,44 @@ export default class SLide {
       const margin = (this.wrapper.offsetWidth - element.offsetWidth) / 2
       const position = -(element.offsetLeft - margin)
       return {
+        element,
         position
       }
     })
     this.moveSlide(this.slideArray[index].position)
     this.values.endPosition = this.slideArray[index].position
-    return this.slideIndex(index)
+    this.slideIndex(index)
+    return this.highlightMainSlide(index)
   }
   slideIndex(index) {
-    const last = this.slideArray.length - 1
     this.positions = {
-      prev: index ? index - 1 : undefined,
+      prev: index -1 === -1 ? undefined : index - 1,
       active: index,
-      next: index === last ? undefined : index + 1
+      next: index + 1 === 6 ? undefined : index + 1
     }
   }
   changeSlide() {
-    return this.values.movement > 0 
-      ? this.enableNextSlide()
-      : this.enablePrevSlide()
+    return this.values.movement > 0
+      ? this.changeNextSlide()
+      : this.changePrevSlide()
   }
-  enableNextSlide() {
-    return this.positions.next !== undefined
-      ? this.centralizeSlide(this.positions.next)
-      : this.centralizeSlide(this.positions.active)
+  changeNextSlide() {
+    return this.positions.next === undefined
+      ? this.centralizeSlide(this.positions.active)
+      : this.centralizeSlide(this.positions.next)
   }
-  enablePrevSlide() {
-    return this.positions.prev !== undefined
-      ? this.centralizeSlide(this.positions.prev)
-      : this.centralizeSlide(this.positions.active)
+  changePrevSlide() {
+    return this.positions.prev === undefined
+      ? this.centralizeSlide(this.positions.active)
+      : this.centralizeSlide(this.positions.prev)
   }
   transition(active) {
     this.slide.style.transition = active ? ".3s" : ""
+  }
+  highlightMainSlide(index) {
+    this.slideArray.forEach((item) => {
+      item.element.classList.remove(this.active)
+    })
+    this.slideArray[index].element.classList.add(this.active)
   }
 }
